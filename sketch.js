bd9ebb9f7dfea9cb5b470edf9a560faab93e14a9
jenna590 <72899390+jenna590@users.noreply.github.com>
2022-03-20 16:57:34 -0400

    v

--- sketch.js
+++ sketch.js
@@ -16,7 +16,6 @@ function preload(){
 
 }
 
-
 function setup() {
   createCanvas(windowWidth, windowHeight);
   angleMode(DEGREES)
@@ -74,64 +73,122 @@ function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
 
-
 function keyTyped(){
   if (key == '0'){
     s00.play();
-    c = color(255, 0, 0);
+    c = color(171, 196, 255);
 
 
   }
   if (key == '1'){
     s01.play();
-    c = color(0, 255, 0);
+    c = color(255, 242, 194);
 
 
   }
   if (key == '2'){
     s02.play();
-    c = color(0, 0, 255);
+    c = color(254, 239, 235);
 
 
   }
   if (key == '3'){
     s03.play();
-    c = color(255, 0, 255);
+    c = color(255, 245, 235);
 
 
   }
   if (key == '4'){
     s04.play();
-    c = color(255, 255, 0);
+    c = color(237, 242, 251);
 
 
   }
   if (key == '5'){
     s05.play();
-    c = color(173, 66, 245);
+    c = color(226, 234, 252);
 
 
   }
   if (key == '6'){
     s06.play();
-    c = color(245, 150, 66);
+    c = color(215, 227, 252);
 
 
   }
   if (key == '7'){
     s07.play();
-    c = color(66, 203, 245);
+    c = color(204, 219, 253);
 
   }
   if (key == '8'){
     s08.play();
-    c = color(245, 96, 66);
+    c = color(193, 211, 254);
 
 
   }
   if (key == '9'){
     s09.play();
-    c = color(216, 235, 52);
+    c = color(182, 204, 254);
+
+
+  }
+  if (key == 'q'){
+    s03.play();
+    c = color(171, 129, 205);
+
+
+  }
+  if (key == 'w'){
+    s04.play();
+    c = color(124, 79, 149);
+
+
+  }
+  if (key == 'e'){
+    s05.play();
+    c = color(77, 29, 93);
+
+
+  }
+  if (key == 'r'){
+    s06.play();
+    c = color(49, 15, 88);
+
+
+  }
+  if (key == 't'){
+    s07.play();
+    c = color(35, 8, 85);
+
+  }
+  if (key == 'y'){
+    s08.play();
+    c = color(20, 1, 82);
+
+
+  }
+  if (key == 'u'){
+    s09.play();
+    c = color(24, 1, 101);
+
+
+  }
+  if (key == 'i'){
+    s09.play();
+    c = color(33, 13, 131);
+
+
+  }
+  if (key == 'o'){
+    s09.play();
+    c = color(41, 24, 161);
+
+
+  }
+  if (key == 'p'){
+    s09.play();
+    c = color(57, 46, 220);
 
 
   }
